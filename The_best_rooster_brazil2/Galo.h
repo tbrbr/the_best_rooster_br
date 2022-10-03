208d030385a59df5fadc55ee49614dc87a4fab80
WalterHenri <walterdoideira5438@gmail.com>
2022-10-03 20:53:10 -0300

    mota n sabe usar o canso do git

--- The_best_rooster_brazil2/Galo.h
+++ The_best_rooster_brazil2/Galo.h
@@ -1,10 +1,10 @@
-#pragma once
+
 #ifndef GALO_H_included
 #define GALO_H_included
 
-
-
+#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 using namespace sf;
 
@@ -12,241 +12,389 @@ using namespace sf;
 #define G 9.81
 #define Euler 2.718281
 
-namespace Rooster {
+
+
+namespace Rooster
+{
+
+/* a element is a small part of a rooster or any caracther available
+that way the animations will be smoother and easy to make for any rooster*/
+
+class Element
+{
+
+public:
+
+
+    sf::Sprite sprite;
+
+    Vector2f size;
+    Vector2f texPos;
+    Vector2f position;
+    Vector2f center;
+    Vector2f attach;
+
+    Vector2f offset;
+
+    int attachId = -1;
+
+    Vector2f scl;
+
+    float angle;
+
+    float otherAngle = 0;
+
+    Vector2f otherPos;
+
+    Element(sf::Texture& text, int xTex, int yTex, int wid, int hei, int xPin, int yPin, int xAttach, int yAttach)
+    {
+
+        sprite.setTexture(text);
+
+
+        // Retangulo do sprite na textura
+        texPos.x = xTex;
+        texPos.y = yTex;
+        size.x = wid;
+        size.y = hei;
+
+        sprite.setTextureRect(sf::IntRect(xTex, yTex, wid, hei));
+
+        // Centro de rotação do sprite
+        center.x = xPin - xTex;
+        center.y = yPin - yTex;
+
+        sprite.setOrigin(center.x, center.y);
+
+
+        // Relative to Center of part this is attached to
+        attach.x = xAttach;
+        attach.y = yAttach;
+
+        // Angulo e escala
+        angle = 0;
+        scl.x = 0.25;
+        scl.y = 0.25;
+
+        offset.x = 0;
+        offset.y = 0;
 
 
 
-    class Element {
 
-    public:
+    }
 
 
-        sf::Sprite sprite;
-        Vector2f position;
-        Vector2f center;
 
-        Element(sf::Texture& text, Vector2i, int wid, int hei, int xPin, int yPin) {
+    void show(sf::RenderWindow& window)
+    {
+        sprite.setPosition(position);
+        sprite.setRotation(angle+otherAngle);
+        sprite.setScale(scl.x,scl.y);
+        sprite.setOrigin(center.x, center.y);
+        window.draw(sprite);
 
-            sprite.setTexture(text);
-            sprite.setTextureRect(sf::IntRect(xTex, yTex, wid, hei));
+    }
 
-            this->xPin = xPin - xTex;
-            this->yPin = yPin - yTex;
 
-            xOffset = 0;
-            yOffset = 0;
-            angleOffset = 0;
 
+    /// Updates element position based on the Part it's attached to
+    void update(float otherX, float otherY, float otherAng)
+    {
+
+        float fixAngle = 3.141592*otherAng/180;
+
+        /// Calculating actualized position...
+        position.x = offset.x + otherX + scl.x*(attach.x*cos(fixAngle) - attach.y*sin(fixAngle)) ;
+        position.y = offset.y + otherY + scl.y*(attach.x*sin(fixAngle) + attach.y*cos(fixAngle));
+
+        otherAngle = otherAng;
+
+    }
+};
+
+
+
+enum state
+{
+    STOPPED = 0,
+    RUNNING,
+    LOW_KICK,
+    HIGH_KICK,
+    SLAP,
+    JUMP,
+    JUMP_KICK,
+    SPECIAL,
+    DEFENDING,
+    RIGHT
+
+};
+enum roosters
+{
+    BOTAS,
+    SNIPER,
+    KALSA,
+    PESTE_NEGRA,
+    BRUXO
+};
+
+enum bodyParts{
+     CORPO,
+     CABECA,
+     RABO,
+     ASA_FRENTE,
+     ASA_ATRAS,
+     PERNA_FRENTE,
+     PE_FRENTE,
+     PERNA_ATRAS,
+     PE_ATRAS
+
+};
+
+typedef struct
+{
+    Vector2i xCenter;
+    int radius;
+} HitBox;
+
+class Galo
+{
+    HitBox hitbox;
+    int hp;
+    int id;
+    float peso;
+    int atk;
+    int def;
+    int speed;
+    int estado;
+
+    bool air;
+    float hspeed;
+    float vspeed;
+
+    float legWalkAngFase = 0;
+    float ArmSpinAngFase = 0;
+    float Arm2SpinAngFase = 0;
+
+    int frames = 0;
+
+    Sprite * sprite = new Sprite[9];
+    RectangleShape r;
+
+
+    std::vector<int> elementDrawOrder;
+    std::vector<Element*> elementos;
+
+public:
+
+    bool facingRight = false;
+    bool attacking = false;
+
+    void addElement(sf::Texture& tex, float xTex, float yTex, float wid,
+                    float hei, float xCenter, float yCenter, float xAttach,
+                    float yAttach, int idAttach){
+
+        float xAt = 0;
+        float yAt = 0;
+        if(idAttach != -1)
+        {
+            xAt = xAttach - (elementos.at(idAttach)->texPos.x + elementos.at(idAttach)->center.x);
+            yAt = yAttach - (elementos.at(idAttach)->texPos.y + elementos.at(idAttach)->center.y);
         }
 
-        void show(sf::RenderWindow& window) {
-            sprite.setPosition(x + xOffset - xPin + xAttach, y + yOffset - yPin + yAttach);
-            sprite.setRotation(angle + angleOffset);
+        Element* part = new Element(tex, xTex, yTex, wid, hei, xCenter, yCenter, xAt, yAt);
+        part->attachId = idAttach;
+        elementos.push_back(part);
+    }
 
-            sf::RectangleShape rectan(Vector2f(sprite.getGlobalBounds().width, sprite.getGlobalBounds().height));
-            rectan.setPosition(sprite.getPosition());
-            rectan.setFillColor(Color(0, 0, 0, 0));
-            rectan.setOutlineThickness(4);
-            rectan.setOutlineColor(Color::White);
-            window.draw(sprite);
-            window.draw(rectan);
+    Galo(HitBox _hitbox, int atk, int def, int speed, int _state, Texture& _texture)
+    {
+        hitbox = _hitbox;
+        this->atk = atk;
+        this->def = def;
+        this->speed = speed;
+        this->estado = _state;
+        this->peso = 2;
+        this->air = false;
+        this->hspeed = 0;
+        this->vspeed = 0;
+        r.setSize(Vector2f(20, 20));
+        r.setPosition(400, 600);
 
-            sf::CircleShape circle(6);
-            circle.setFillColor(Color::Red);
-            circle.setPosition(x - xPin, y - yPin);
-            window.draw(circle);
+
+
+        /// Criando Partes do corpo
+        addElement(_texture, 26, 517, 479, 461, 250, 712, 0, 0, -1); // Corpo
+
+        addElement(_texture, 64, 38, 318, 434, 243, 420, 138, 595, 0); // Cabeça
+
+        addElement(_texture, 517, 565, 460, 517, 628, 834, 435, 824, 0); // Rabo
+
+        addElement(_texture, 464, 264, 331, 233, 748, 380, 300, 670, 0); // AsaFrente
+
+        addElement(_texture, 464, 264, 331, 233, 748, 380, 150, 700, 0); // AsaTrás
+
+        addElement(_texture, 100, 993, 144, 157, 202, 1015, 387, 930, 0); // Perna Frente
+
+        addElement(_texture, 7, 1229, 260, 186, 173, 1242, 130, 1134, 5); // pé frente
+
+        addElement(_texture, 100, 993, 144, 157, 202, 1015, 260, 910, 0); // Perna trás
+
+        addElement(_texture, 7, 1229, 260, 186, 173, 1242, 130, 1134, 7); // pé trás
+
+
+        elementDrawOrder.push_back(RABO);
+        elementDrawOrder.push_back(ASA_ATRAS);
+        elementDrawOrder.push_back(PE_ATRAS);
+        elementDrawOrder.push_back(PERNA_ATRAS);
+        elementDrawOrder.push_back(PE_FRENTE);
+        elementDrawOrder.push_back(PERNA_FRENTE);
+        elementDrawOrder.push_back(CORPO);
+        elementDrawOrder.push_back(CABECA);
+        elementDrawOrder.push_back(ASA_FRENTE);
+
+    }
+
+    RectangleShape getSprite()
+    {
+        return r;
+    }
+    void setState(state estado)
+    {
+        this->estado = estado;
+    }
+    void setState(int estado)
+    {
+        this->estado = estado;
+    }
+    void animJump()
+    {
+        if (!air)
+        {
+            vspeed += (peso * (-8)) / 2;
+            air = true;
         }
+    }
+    void animRun()
+    {
+        float acc = 0.5;
 
-        void update(float x, float y, float angle) {
-            this->x = x;
-            this->y = y;
-            this->angle = angle;
+        if (facingRight)
+        {
+            hspeed = (hspeed + acc) >  10 ?  10 : (hspeed + acc);
+            for(int i = 0; i < elementos.size(); i ++){
+                elementos.at(i)->scl.x = -0.25;
+
+            }
         }
-    };
+        else
+        {
+            hspeed = (hspeed - acc) < -10 ? -10 : (hspeed - acc);
+            for(int i = 0; i < elementos.size(); i ++){
+                elementos.at(i)->scl.x = 0.25;
 
-
-    class Cock: Element{
-
-        struct body {
-            sf::Sprite sprite;
-            Vector2f center;
-
-        };
-
-        struct head {
-            sf::Sprite sprite;
-            Vector2f center;
-        };
-
-        struct leftArm {
-            sf::Sprite sprite;
-            Vector2f center;
-        };
-
-        struct rightArm {
-            sf::Sprite sprite;
-            Vector2f center;
-        };
-
-        struct leftLeg {
-            sf::Sprite sprite;
-            Vector2f center;
-        };
-
-        struct rightLeg {
-            sf::Sprite sprite;
-            Vector2f center;
-        };
-
-        struct tail {
-            sf::Sprite sprite;
-            Vector2f center;
-        };
-
-
-
-
-    };
-
-
-
-
-
-
-    enum state {
-        STOPPED = 9,
-        RUNNING,
-        LOW_KICK,
-        HIGH_KICK,
-        SLAP,
-        JUMP,
-        JUMP_KICK,
-        SPECIAL,
-        DEFENDING,
-        RIGHT
-
-    };
-    enum roosters {
-        BOTAS,
-        SNIPER,
-        KALSA,
-        PESTE_NEGRA,
-        BRUXO
-    };
-
-    typedef struct {
-        Vector2i xCenter;
-        int radius;
-    }HitBox;
-
-    class Galo {
-        HitBox hitbox;
-        int hp;
-        int id;
-        float peso;
-        int atk;
-        int def;
-        int speed;
-        int estado;
-        bool air;
-
-        float hspeed;
-        float vspeed;
-
-
-
-        Sprite* sprite = new Sprite[9];
-        RectangleShape r;
-
-
-
-        std::vector<Element*> elementos;
-
-    public:
-
-        Galo(HitBox _hitbox, int atk, int def, int speed, int _state, Texture& _texture) {
-            hitbox = _hitbox;
-            this->atk = atk;
-            this->def = def;
-            this->speed = speed;
-            this->estado = _state;
-            this->peso = 2;
-            this->air = false;
-            this->hspeed = 0;
-            this->vspeed = 0;
-            r.setSize(Vector2f(20, 20));
-            r.setPosition(400, 600);
-
-            Element* galoBody = new Element(_texture, 26, 517, 479, 461, 250, 712);
-            elementos.push_back(galoBody);
-
-            Element* galoHead = new Element(_texture, 64, 38, 318, 434, 243, 420);
-            galoHead->parentId = 0;
-            galoHead->xAttach = -100 + (elementos.at(0)->sprite.getTextureRect().left + elementos.at(0)->xPin);
-            galoHead->yAttach = -530 + (elementos.at(0)->sprite.getTextureRect().top + elementos.at(0)->yPin);
-
-            elementos.push_back(galoHead);
-
-
-
-
-
-        }
-
-        RectangleShape getSprite() {
-            return r;
-        }
-        void setState(state estado) {
-            this->estado = estado;
-        }
-        void setState(int estado) {
-            this->estado = estado;
-        }
-        void animJump() {
-
-            if (!air) {
-                this->vspeed += (peso * (-8)) / 2;
-                air = true;
             }
 
-
-        }
-        void animRun() {
-            if (this->estado - 18 >= 0)
-                hspeed += 1;
-            else {
-                hspeed += -1;
-            }
-
         }
 
-        void show(sf::RenderWindow& window) {
-            for (int i = 0; i < elementos.size(); i++) {
-                elementos.at(i)->show(window);
+    }
 
-            }
+    void setHspeed(float spd)
+    {
+        hspeed = spd;
+    }
+
+    void show(sf::RenderWindow& window)
+    {
+
+        for(int i = 0; i < elementDrawOrder.size(); i++)
+        {
+            elementos.at(elementDrawOrder.at(i))->show(window);
+        }
+    }
+
+    void inline update(int mx, int my)
+    {
+
+        if(air)
+        {
+            vspeed += peso * G/100;
         }
 
-        void inline update(int mx, int my) {
-
-            this->vspeed += peso * G / 100;
-
-            if (this->r.getPosition().y > 600) {
-                this->vspeed = 0;
-                this->r.setPosition(this->r.getPosition().x, 600);
-                air = false;
-            }
-
-            this->r.move(this->hspeed, this->vspeed);
-
-
-            elementos.at(0)->update(mx, my, 0);
-
-            //elementos.at(1)->update(elementos.at(0)->x, elementos.at(0)->y, elementos.at(0)->angle);
-
-
+        if (r.getPosition().y > 600)
+        {
+            vspeed = 0;
+            r.setPosition(r.getPosition().x, 600);
+            air = false;
         }
-    };
+
+        r.move(hspeed, vspeed);
+
+        /// Animação das partes do corpo
+
+        //these parts moves all the time to create weather like animations
+        frames++;
+
+        elementos.at(CORPO)->angle += 0;
+        elementos.at(CORPO)->update(r.getPosition().x, r.getPosition().y, 0);
+
+        elementos.at(RABO)->angle = sin(frames/200.f)*20;
+
+        /******************************************************************/
+
+        if(attacking)
+        {
+            ArmSpinAngFase += 10;
+            ArmSpinAngFase -= ((int)ArmSpinAngFase/360)*360;
+        }
+        else
+        {
+            ArmSpinAngFase *= 0.9;
+            Arm2SpinAngFase *= 0.9;
+        }
+
+        //jump animation
+        if(air){
+            ArmSpinAngFase = (vspeed/8) * 45;
+            Arm2SpinAngFase =(vspeed/8) * 45;
+
+            elementos.at(PERNA_FRENTE)->attach.y += vspeed/2;
+            elementos.at(PE_FRENTE)->angle += vspeed/20;
+            elementos.at(PERNA_ATRAS)->attach.y += vspeed/4;
+            elementos.at(PE_ATRAS)->angle += vspeed/20;
+        }
+
+        elementos.at(ASA_FRENTE)->angle = ArmSpinAngFase;
+        elementos.at(ASA_FRENTE)->offset.y = sin(frames/200.f)*5;
+        elementos.at(ASA_ATRAS)->angle = Arm2SpinAngFase;
+        elementos.at(ASA_ATRAS)->offset.y = sin(frames/200.f)*5;
+
+        //running animation
+        if(estado == RUNNING)
+        {
+            legWalkAngFase += hspeed;
+            legWalkAngFase -= ((int)legWalkAngFase/360)*360;
+        }
+        else
+        {
+            legWalkAngFase *= 0.8;
+        }
+
+
+        elementos.at(PERNA_FRENTE)->angle = sin(2*PI*legWalkAngFase/360)*60;
+        elementos.at(PERNA_ATRAS)->angle = -sin(2*PI*legWalkAngFase/360)*60;
+
+
+        for(int i = 1; i < elementos.size(); i++)
+        {
+
+            Element* elem = elementos.at(elementos.at(i)->attachId);
+            elementos.at(i)->update(elem->position.x, elem->position.y, elem->angle + elem->otherAngle);
+        }
+
+
+    }
+};
 
 
 }
--- The_best_rooster_brazil2/main.cpp
+++ The_best_rooster_brazil2/main.cpp
@@ -10,7 +10,10 @@
 using namespace sf;
 using namespace Rooster;
 
-int main() {
+
+
+
+int main(){
 
     RenderWindow* window = new RenderWindow(VideoMode(1280, 720), "TBRB");
     window->setFramerateLimit(FRAMERATE_LIMIT);
@@ -38,29 +41,63 @@ int main() {
                 }
                 if (e.key.code == Keyboard::A) {
                     galo.setState(Rooster::RUNNING);
+                    galo.facingRight = false;
                     galo.animRun();
                 }
                 if (e.key.code == Keyboard::D) {
-                    galo.setState(Rooster::state::RUNNING + Rooster::RIGHT);
+                    galo.setState(Rooster::state::RUNNING);
+                    galo.facingRight = true;
                     galo.animRun();
                 }
 
             }
 
-            if (e.type == Event::MouseMoved) {
-
+            if(e.type == Event::KeyReleased){
+                if (e.key.code == Keyboard::A) {
+                    galo.setState(Rooster::RUNNING);
+                    galo.facingRight = false;
+                    galo.animRun();
+                }
+                if (e.key.code == Keyboard::D) {
+                }
+            }
+            if(e.type == Event::MouseMoved){
                 mouseX = e.mouseMove.x;
                 mouseY = e.mouseMove.y;
             }
-
         }
+
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+            galo.setState(Rooster::state::RUNNING);
+            galo.facingRight = true;
+            galo.animRun();
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+            galo.setState(Rooster::state::RUNNING);
+            galo.facingRight = false;
+            galo.animRun();
+        } else {
+            galo.setState(Rooster::state::STOPPED);
+            galo.setHspeed(0);
+        }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
+            galo.attacking = true;
+        } else {
+            galo.attacking = false;
+        }
+
 
         galo.update(mouseX, mouseY);
 
 
         window->clear();
+        RectangleShape fundo(Vector2f(1280, 720));
+        fundo.setPosition(0,0);
+        fundo.setFillColor(Color(100, 100, 100));
+
+        window->draw(fundo);
         galo.show(*window);
-        window->draw(galo.getSprite());
         window->display();
 
     }
